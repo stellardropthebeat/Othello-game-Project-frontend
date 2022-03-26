64be2f92e34414dd98e24d4567882441cae850db
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-03-26 11:49:02 +0700

    Create lobby.vue in order to allow user get in to lobby

---
+++ src/views/LobbyView.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <template>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="playerName"
+          :rules="playerNameRules"
+          label="Username"
+          required
+          ></v-text-field>
+
+        <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="apply"
+        > apply
+        </v-btn>
+      </v-form>
+    </template>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    playerName: "",
+    playerNameRules: [v => !!v || "Player Name is required"],
+  }),
+
+  methods:{
+    async apply() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
