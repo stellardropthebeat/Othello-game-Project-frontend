fe135aa6c175078cef4b1aac38454ebf608f1087
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-03-28 16:24:10 +0700

    Try creating table with a data inside

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,39 +1,69 @@
 <template>
-  <v-container>
-    <template>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <v-text-field
-          v-model="playerName"
-          :rules="playerNameRules"
-          label="Username"
-          required
-          ></v-text-field>
-
-        <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="apply"
-        > apply
-        </v-btn>
-      </v-form>
-    </template>
-  </v-container>
+  <v-data-table
+      :headers="headers"
+      :items="details"
+      :items-per-page="5"
+      class="elevation-1"
+  ></v-data-table>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    valid: true,
-    playerName: "",
-    playerNameRules: [v => !!v || "Player Name is required"],
-  }),
-
-  methods:{
-    async apply() {
-
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Lobby Name',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Host Name', value: 'host_name' },
+      ],
+      details: [
+        {
+          name: 'Player 1 lobby',
+          host_name: "Player 1",
+        },
+        {
+          name: 'Player 2 lobby',
+          host_name: "Player 2",
+        },
+        {
+          name: 'Player 3 lobby',
+          host_name: "Player 3",
+        },
+        {
+          name: 'Player 4 lobby',
+          host_name: "Player 4",
+        },
+        {
+          name: 'Player 5 lobby',
+          host_name: "Player 5",
+        },
+        {
+          name: 'Player 6 lobby',
+          host_name: "Player 6",
+        },
+        {
+          name: 'Player 7 lobby',
+          host_name: "Player 7",
+        },
+        {
+          name: 'Player 8 lobby',
+          host_name: "Player 8",
+        },
+        {
+          name: 'Player 9 lobby',
+          host_name: "Player 9",
+        },
+        {
+          name: 'Player 10 lobby',
+          host_name: "Player 10",
+        },
+      ],
     }
-  }
+  },
 }
 </script>
 
