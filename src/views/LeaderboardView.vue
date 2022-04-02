e6fe3f3bc1e9043f5b0811d217cbf63043e8086e
ThanthongFifa <Fifa0962@gmail.com>
2022-04-02 20:11:37 +0700

    added leaderboard and link REPLAY LATEST GAME to proper page

--- src/views/LeaderboardView.vue
+++ src/views/LeaderboardView.vue
@@ -1,21 +1,55 @@
 <template>
-  <v-container>
-     <h1>
-       Leaderboard
-     </h1>
-
-  </v-container>
+  <div class="container">
+    <h1 class="mt-4 text-center">Leaderboard</h1>
+    <table class="table mt-5 box">
+      <thead>
+      <tr>
+        <th scope="col" class="column">#</th>
+        <th scope="col" class="column">Name</th>
+        <th scope="col" class="column">Score</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(entry, i) in allScores" :key="i">
+        <th scope="row">{{ ++i }}</th>
+        <td>{{ entry[0] }}</td>
+        <td>{{ entry[1] }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-
 <script>
-export default {
-  components: {},
+import Vue from "vue";
 
-  methods: {},
+export default {
+  data: () => ({ name: "", score: "", allScores: [] }),
+  computed: {
+  },
+  async mounted() {
+    let response = await Vue.axios.get("/api/leaderboard");
+    this.allScores = response.data.userList
+    console.log(response.data.userList)
+  },
+  methods: {
+    async onEntry() {
+      let response = await Vue.axios.get("/api/leaderboard");
+      this.allScores=response.data.userList
+      console.log(response.data.userList)
+    },
+  },
 };
 </script>
 
 <style scoped>
+.column {
+  margin: auto;
+  padding: 50px;
+}
+.box {
+  margin: auto;
+}
+
 
 </style>
