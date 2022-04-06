846f5a226630d60aab63edbc0b25c011c3e7fdbc
ThanthongFifa <Fifa0962@gmail.com>
2022-04-06 09:24:49 +0700

    write something on credit page, fix login and sign up, make leaderboard center

--- src/views/CreditView.vue
+++ src/views/CreditView.vue
@@ -1,11 +1,30 @@
 <template>
   <v-container>
-     <h1>
-       Credit
-     </h1>
-    <h2>
-      Bam is cute, and looking for BF
-    </h2>
+    <div class="text-center">
+      <h1>
+        Credit
+      </h1>
+      <h2>
+        Team Members
+      </h2>
+      <v-card-text>
+        Niraporn Kovitaya 6281507
+      </v-card-text>
+      <v-card-text>
+        Nutchapol Isariyapruit 6281454
+      </v-card-text>
+      <v-card-text>
+        Thanthong Chim-ong 6280024
+      </v-card-text>
+
+      <h2>
+        Adviser
+      </h2>
+      <v-card-text>
+        Weerapong Phadungsukanan
+      </v-card-text>
+    </div>
+
 
   </v-container>
 </template>
